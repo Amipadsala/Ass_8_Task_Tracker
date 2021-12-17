970ccac871fd37043759a70fd29ef85d97004126
Amipadsala <91412262+Amipadsala@users.noreply.github.com>
2021-12-17 23:34:00 +0530

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; Ami Padsala</p>
+      <Link to="/About">About</Link>
+    </footer>
+  );
+};
+
+export default Footer;
